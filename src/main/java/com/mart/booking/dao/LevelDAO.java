11967b036934afba67b9a3b6033fb43b158bb734
Rameshor Pathak <rmsor.pth02@gmail.com>
2016-06-18 22:06:05 -0400

    changed DAO name

--- src/main/java/com/mart/booking/dao/LevelDAO.java
+++ src/main/java/com/mart/booking/dao/LevelDAO.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import com.mart.booking.domain.Level;
 
-public interface LevelDAO {
+public interface LevelDao {
 	
 	 /**
 	  * returs list of all levels of seats in venue
